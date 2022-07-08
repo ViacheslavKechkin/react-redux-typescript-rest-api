3c54881c0167988570bf5923903ab95de033c8b8
Вячеслав Кечкин <vaceslavkeckin@Mac-mini-Vaceslav.local>
2022-07-08 11:30:43 +0300

    add obj export and fix services

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,12 +4,20 @@ import { requestInstance } from "../utils";
 
 import { TPost, TDto } from "../types"
 
+type Config = {
+  data: {
+    params: TDto
+  }
+}
+
 const getPost = (id: IGetPost) =>
   requestInstance
     .get<TPost>(`posts/${id}`)
     .then((res) => res.data);
 
-const getPosts = (id: IGetPosts, config: TDto) =>
+const getPosts = (id: IGetPosts, config: Config) =>
   requestInstance
-    .get<TPost>(`posts/${id}`)
+    .get<TPost[]>((`posts/${id}`), config)
     .then((res) => res.data);
+
+export const postService = { getPost, getPosts };
