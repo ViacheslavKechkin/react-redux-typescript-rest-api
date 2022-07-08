e0d72f79ed1537eb65aa80c5414a1fb6560ef61f
Вячеслав Кечкин <vaceslavkeckin@Mac-mini-Vaceslav.local>
2022-07-08 13:59:06 +0300

    add thunk for posts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,22 +2,24 @@ import { IGetPost, IGetPosts } from "../interfaces";
 
 import { requestInstance } from "../utils";
 
-import { TPost, TDto } from "../types"
+import { TPost, TDto } from "../types";
 
 type Config = {
   data: {
-    params: TDto
-  }
-}
+    params: TDto;
+  };
+};
 
 const getPost = (id: IGetPost) =>
   requestInstance
     .get<TPost>(`posts/${id}`)
-    .then((res) => res.data);
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
 
 const getPosts = (id: IGetPosts, config: Config) =>
   requestInstance
-    .get<TPost[]>((`posts/${id}`), config)
-    .then((res) => res.data);
+    .get<TPost[]>(`posts/${id}`, config)
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
 
 export const postService = { getPost, getPosts };
--- src/store/postSlice.ts
+++ src/store/postSlice.ts
@@ -6,6 +6,17 @@ import { initDetail, initArray } from "../const"
 
 import { TPost, TSlice } from "../types"
 
+import { postService } from "../services"
+
+const { getPost, getPosts } = postService;
+
+const fetchPosts = createAsyncThunk(
+  "post/posts",
+  async (_, dispatch) => {
+    await getPosts
+  }
+)
+
 const initialState: TSlice<TPost> = {
   detail: { ...initDetail },
   list: { ...initArray },
@@ -15,13 +26,29 @@ const postSlice = createSlice({
   name: "post",
   initialState,
   reducers: {
-    getPosts(state, action: PayloadAction<TPost>) {
+    getPostsTest(state, action: PayloadAction<TPost>) {
       state.detail.result = action.payload;
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state: TSlice<TPost>, action) => {
+        state.list.fetching = true;
+      })
+      .addCase(fetchPosts.fulfilled, (state: TSlice<TPost>, action) => {
+        state.list.fetching = false;
+        state.list.success = true;
+        state.list.result = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state: TSlice<TPost>, action) => {
+        state.list.fetching = false;
+        state.list.success = true;
+        state.list.message = action.meta.requestStatus;
+      })
+  }
 });
 
 export default postSlice.reducer;
 
-export const { getPosts } = postSlice.actions;
+export const { getPostsTest } = postSlice.actions;
 
