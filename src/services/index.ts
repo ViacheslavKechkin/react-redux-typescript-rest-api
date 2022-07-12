ab0d6d2eb00ac0d6a5680da31a08aff12bb3febe
Вячеслав Кечкин <vaceslavkeckin@Mac-mini-Vaceslav.local>
2022-07-12 18:18:55 +0300

    add delete post

--- src/services/index.ts
+++ src/services/index.ts
@@ -12,8 +12,17 @@ const getPost = (id: number) =>
   requestInstance
     .get<TPost>(`posts/${id}`);
 
+const deletePost = (id: number) =>
+  requestInstance
+    .delete<TPost>(`posts/${id}`);
+
+const updatePost = (id: number, config: Partial<TPost>) =>
+  requestInstance
+    .patch<TPost>(`posts/${id}`, config);
+
 const getPosts = (id: number, config: Partial<Config>) =>
   requestInstance
-    .get<TPost[]>(`posts/${id}`, config);
+    .get<TPost[]>(`posts/list`, config);
 
-export const postService = { getPost, getPosts };
+
+export const postService = { getPost, getPosts, deletePost, updatePost };
--- src/store/postSlice.ts
+++ src/store/postSlice.ts
@@ -1,22 +1,30 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { requestInstance } from "../utils";
-
 import { initDetail, initArray } from "../const"
 
 import { TPost, TSlice } from "../types"
 
 import { postService } from "../services"
 
-import type WritableDraft from "immer";
-
-const { getPost, getPosts } = postService;
+const { getPost, getPosts, deletePost, updatePost } = postService;
 
 const getPostsThunk = createAsyncThunk(
   "post/posts",
   async () => {
     try {
       await getPosts
+    }
+    catch (e) {
+      console.error(e)
+    }
+  }
+)
+
+const deletePostThunk = createAsyncThunk(
+  "post/deletePost",
+  async (id: number) => {
+    try {
+      await deletePost
     }
     catch (e) {
       console.error(e)
@@ -40,13 +48,27 @@ const postSlice = createSlice({
         state.list.success = false;
       })
       .addCase(getPostsThunk.fulfilled, (state, action) => {
-        state.list.fetching = false;
-        state.list.success = false;
+        state.list.fetching = true;
+        state.list.success = true;
         state.list.result = action.payload;
       })
       .addCase(getPostsThunk.rejected, (state, action) => {
         state.list.fetching = false;
+        state.list.success = false;
+        state.list.message = action.meta.requestStatus;
+      })
+      .addCase(deletePostThunk.pending, (state, action) => {
+        state.list.fetching = true;
+        state.list.success = false;
+      })
+      .addCase(deletePostThunk.fulfilled, (state, action) => {
+        state.list.fetching = true;
         state.list.success = true;
+        state.list.result = action.payload;
+      })
+      .addCase(deletePostThunk.rejected, (state, action) => {
+        state.list.fetching = false;
+        state.list.success = false;
         state.list.message = action.meta.requestStatus;
       })
   }
